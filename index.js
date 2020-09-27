c492d937290398815028dda35fa0315b3fe68f8b
iamsaief <saiefalemon@gmail.com>
2020-09-28 04:17:09 +0600

    🤘 api created:get,post,delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
 
 const mongoPassword = "uKWhQu6YPc2pyME";
 
@@ -20,12 +21,29 @@ client.connect((err) => {
 	const collection = client.db("mongocruddb").collection("products");
 	console.log("DB connected 🥳");
 
+	app.get("/products", (req, res) => {
+		collection.find({}).toArray((err, docs) => {
+			res.send(docs);
+		});
+	});
+
+	app.get("/product/:id", (req, res) => {
+		collection.find({ _id: ObjectId(req.params.id) }).toArray((err, docs) => {
+			res.send(docs[0]);
+		});
+	});
+
 	app.post("/addProduct", (req, res) => {
 		const product = req.body;
 		collection.insertOne(product).then((result) => {
 			console.log("Product Inserted ✅");
 			res.send("Success 🤘");
 		});
+	});
+
+	app.delete("/delete/:id", (req, res) => {
+		console.log(req.params.id);
+		collection.deleteOne({ _id: ObjectId(req.params.id) }).then((result) => console.log(result));
 	});
 
 	// client.close();
